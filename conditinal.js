31da81dbc8af6a2da5b97c8f96be272a18c17dcd
Omar Adem <omardin.ahmed@gmail.com>
2024-07-05 17:37:40 -0700

    Conditinal Practice

---
+++ conditinal.js
@@ -0,0 +1,17 @@
+function rewardMySelf(workoutFinishOnTime){
+  if (workoutFinishOnTime < 10){
+    console.log("I want juce");  
+  }
+  else if (workoutFinishOnTime >= 10 && workoutFinishOnTime < 16){
+    console.log("I want hot chocolate");
+  }
+  else if (workoutFinishOnTime <= 16 && workoutFinishOnTime < 22){
+    console.log("I want ice cream");
+  }
+  else if (workoutFinishOnTime >= 22){
+    console.log("I don't need anything other than sleep!!!")
+  }
+
+}
+
+rewardMySelf(9)
